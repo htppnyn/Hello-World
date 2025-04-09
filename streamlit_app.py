cadb418187742044aab5ff26486eff70a7d4aaa3
hphummar <104311649+htppnyn@users.noreply.github.com>
2025-04-09 03:27:11 +0000

    Test1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Hello World 57!")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+st.write(
+    "Hello! I'm from Thailand. 🚀"
 )
